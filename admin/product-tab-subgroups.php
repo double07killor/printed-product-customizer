a52a1467fe9ba890918fdda003787539a39ff83c
double07killor <75599140+double07killor@users.noreply.github.com>
2025-08-07 16:23:37 -0500

    Make admin groups collapsible

--- admin/product-tab-subgroups.php
+++ admin/product-tab-subgroups.php
@@ -34,53 +34,59 @@ function fpc_subgroups_product_data_panel() {
     <div id="fpc_subgroups_panel" class="panel woocommerce_options_panel hidden">
         <div class="fpc-repeatable-wrapper">
             <div class="fpc-repeatable-container">
-                <div class="fpc-repeatable-row fpc-template" style="display:none;">
-                    <p class="form-field">
-                        <label><?php _e('Label', 'printed-product-customizer'); ?></label>
-                        <input type="text" class="short fpc-generate-key" name="fpc_subgroups[__INDEX__][label]" />
-                    </p>
-                    <p class="form-field">
-                        <label><?php _e('Subgroup Key', 'printed-product-customizer'); ?></label>
-                        <input type="text" class="short fpc-key-field" name="fpc_subgroups[__INDEX__][key]" />
-                    </p>
-                    <p class="form-field">
-                        <label><?php _e('Allowed Filament Groups', 'printed-product-customizer'); ?></label>
-                        <input type="text" class="fpc-tag-input" data-options="<?php echo $filament_keys_json; ?>" name="fpc_subgroups[__INDEX__][allowed]" />
-                    </p>
-                    <p class="form-field">
-                        <label><?php _e('Allow Additional Groups', 'printed-product-customizer'); ?></label>
-                        <input type="checkbox" name="fpc_subgroups[__INDEX__][allow_additional]" value="1" />
-                    </p>
-                    <p class="form-field">
-                        <label><?php _e('Base Grams', 'printed-product-customizer'); ?></label>
-                        <input type="number" step="any" class="short" name="fpc_subgroups[__INDEX__][base_grams]" />
-                    </p>
-                    <p><button type="button" class="button fpc-repeatable-remove"><?php _e('Remove', 'printed-product-customizer'); ?></button></p>
-                </div>
-                <?php foreach ($subgroups as $index => $sg) : ?>
-                    <div class="fpc-repeatable-row">
+                <div class="fpc-repeatable-row fpc-template" data-default-title="<?php esc_attr_e('Subgroup', 'printed-product-customizer'); ?>" style="display:none;">
+                    <h4 class="fpc-group-toggle"><span class="fpc-group-title"><?php _e('Subgroup', 'printed-product-customizer'); ?></span></h4>
+                    <div class="fpc-group-fields">
                         <p class="form-field">
                             <label><?php _e('Label', 'printed-product-customizer'); ?></label>
-                            <input type="text" class="short fpc-generate-key" name="fpc_subgroups[<?php echo esc_attr($index); ?>][label]" value="<?php echo esc_attr($sg['label'] ?? ''); ?>" />
+                            <input type="text" class="short fpc-generate-key" name="fpc_subgroups[__INDEX__][label]" />
                         </p>
                         <p class="form-field">
                             <label><?php _e('Subgroup Key', 'printed-product-customizer'); ?></label>
-                            <input type="text" class="short fpc-key-field" name="fpc_subgroups[<?php echo esc_attr($index); ?>][key]" value="<?php echo esc_attr($sg['key'] ?? ''); ?>" />
+                            <input type="text" class="short fpc-key-field" name="fpc_subgroups[__INDEX__][key]" />
                         </p>
                         <p class="form-field">
                             <label><?php _e('Allowed Filament Groups', 'printed-product-customizer'); ?></label>
-                            <?php $allowed = isset($sg['allowed']) && is_array($sg['allowed']) ? implode(', ', $sg['allowed']) : ''; ?>
-                            <input type="text" class="fpc-tag-input" data-options="<?php echo $filament_keys_json; ?>" name="fpc_subgroups[<?php echo esc_attr($index); ?>][allowed]" value="<?php echo esc_attr($allowed); ?>" />
+                            <input type="text" class="fpc-tag-input" data-options="<?php echo $filament_keys_json; ?>" name="fpc_subgroups[__INDEX__][allowed]" />
                         </p>
                         <p class="form-field">
                             <label><?php _e('Allow Additional Groups', 'printed-product-customizer'); ?></label>
-                            <input type="checkbox" name="fpc_subgroups[<?php echo esc_attr($index); ?>][allow_additional]" value="1" <?php checked(!empty($sg['allow_additional'])); ?> />
+                            <input type="checkbox" name="fpc_subgroups[__INDEX__][allow_additional]" value="1" />
                         </p>
                         <p class="form-field">
                             <label><?php _e('Base Grams', 'printed-product-customizer'); ?></label>
-                            <input type="number" step="any" class="short" name="fpc_subgroups[<?php echo esc_attr($index); ?>][base_grams]" value="<?php echo esc_attr($sg['base_grams'] ?? ''); ?>" />
+                            <input type="number" step="any" class="short" name="fpc_subgroups[__INDEX__][base_grams]" />
                         </p>
                         <p><button type="button" class="button fpc-repeatable-remove"><?php _e('Remove', 'printed-product-customizer'); ?></button></p>
+                    </div>
+                </div>
+                <?php foreach ($subgroups as $index => $sg) : ?>
+                    <div class="fpc-repeatable-row" data-default-title="<?php esc_attr_e('Subgroup', 'printed-product-customizer'); ?>">
+                        <h4 class="fpc-group-toggle"><span class="fpc-group-title"><?php echo esc_html($sg['label'] ?? __('Subgroup', 'printed-product-customizer')); ?></span></h4>
+                        <div class="fpc-group-fields">
+                            <p class="form-field">
+                                <label><?php _e('Label', 'printed-product-customizer'); ?></label>
+                                <input type="text" class="short fpc-generate-key" name="fpc_subgroups[<?php echo esc_attr($index); ?>][label]" value="<?php echo esc_attr($sg['label'] ?? ''); ?>" />
+                            </p>
+                            <p class="form-field">
+                                <label><?php _e('Subgroup Key', 'printed-product-customizer'); ?></label>
+                                <input type="text" class="short fpc-key-field" name="fpc_subgroups[<?php echo esc_attr($index); ?>][key]" value="<?php echo esc_attr($sg['key'] ?? ''); ?>" />
+                            </p>
+                            <p class="form-field">
+                                <label><?php _e('Allowed Filament Groups', 'printed-product-customizer'); ?></label>
+                                <?php $allowed = isset($sg['allowed']) && is_array($sg['allowed']) ? implode(', ', $sg['allowed']) : ''; ?>
+                                <input type="text" class="fpc-tag-input" data-options="<?php echo $filament_keys_json; ?>" name="fpc_subgroups[<?php echo esc_attr($index); ?>][allowed]" value="<?php echo esc_attr($allowed); ?>" />
+                            </p>
+                            <p class="form-field">
+                                <label><?php _e('Allow Additional Groups', 'printed-product-customizer'); ?></label>
+                                <input type="checkbox" name="fpc_subgroups[<?php echo esc_attr($index); ?>][allow_additional]" value="1" <?php checked(!empty($sg['allow_additional'])); ?> />
+                            </p>
+                            <p class="form-field">
+                                <label><?php _e('Base Grams', 'printed-product-customizer'); ?></label>
+                                <input type="number" step="any" class="short" name="fpc_subgroups[<?php echo esc_attr($index); ?>][base_grams]" value="<?php echo esc_attr($sg['base_grams'] ?? ''); ?>" />
+                            </p>
+                            <p><button type="button" class="button fpc-repeatable-remove"><?php _e('Remove', 'printed-product-customizer'); ?></button></p>
+                        </div>
                     </div>
                 <?php endforeach; ?>
             </div>
